e55669bf7d093fb9884619b2216f4f19411115fd
sagararathi <bravo.sagar@gmail.com>
2015-06-14 15:33:52 -0400

    Update server

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 var express = require('express');
     app = express();
 
+    app.set('port', (process.env.PORT || 5000));
     app.use(express.static(__dirname + '/'));
 
     app.get('/', function(request, response) {
       response.send('Hello World!');
     });
 
-    app.listen(5000, function() {
-      console.log('Node app is running on port', 5000);
-    });
+    app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
