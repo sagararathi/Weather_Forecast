1d48e6ad209b28cc2bbca23ca5b70381c99f192b
sagararathi <bravo.sagar@gmail.com>
2015-06-15 19:05:07 -0400

    Add feature to select units

--- app/controllers/mainCtrl.js
+++ app/controllers/mainCtrl.js
@@ -1,6 +1,7 @@
 weatherApp.controller('weatherCtrl', ['$scope', '$location', 'weatherService', 'appSetting', function ($scope, $location,weatherService , appSetting) {
   $scope.city = weatherService.city;
   $scope.days = weatherService.days;
+  $scope.units = weatherService.units;
 
   $scope.appSetting = appSetting;
 
@@ -10,6 +11,10 @@ weatherApp.controller('weatherCtrl', ['$scope', '$location', 'weatherService', '
 
   $scope.$watch('days', function(){
     weatherService.days = $scope.days;
+  });
+
+  $scope.$watch('units', function(){
+    weatherService.units = $scope.units;
   });
 
   $scope.submit = function(){
