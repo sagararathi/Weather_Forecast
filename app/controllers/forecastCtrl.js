b77874036b1b742a514233aa0765433c7de53a4f
sagararathi <bravo.sagar@gmail.com>
2015-06-15 19:06:16 -0400

    Add feature to send respect with required units to server

--- app/controllers/forecastCtrl.js
+++ app/controllers/forecastCtrl.js
@@ -2,12 +2,13 @@ weatherApp.controller('forecastCtrl',['$scope', '$resource', '$log','weatherServ
 
   $scope.city = weatherService.city;
   $scope.days = weatherService.days;
+  $scope.units = weatherService.units;
 
   $scope.appSetting = appSetting;
 
   $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily", { callback: "JSON_CALLBACK"}, {get: {method: "JSONP"}});
 
-  $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, units: 'imperial', cnt: $scope.days});
+  $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, units: $scope.units, cnt: $scope.days});
 
   $scope.convertToDate = function(dateTime){
     return new Date(dateTime * 1000);
